a8b64e8a05099e2f6d1234044e040b8f34aa3f30
RehaS <beratson@gmail.com>
2020-07-18 16:44:11 +0200

    Grid module added, tasklist updated

---
+++ modules/grid.py
@@ -0,0 +1,31 @@
+'''
+  Grid Module
+'''
+from pprint import pprint
+class Grid():
+  def __init__(self, width: int, length: int):
+    self.width = width
+    self.length = length
+    self.grid = self.initialize_grid(width, length)
+
+  def __repr__(self):
+    return str(self.grid)
+
+  def initialize_grid(self, wide: int, long: int):
+    '''Initalizes empty grid with sizes'''
+    arr = [[0 for x in range(wide)] for y in range(long)]
+    return arr
+
+  def display(self):
+    pprint(self.grid)
+
+  def insert_rover(self, locx: int, locy: int, direction: str):
+    # Assumes initial locations are valid
+    if self.is_location_available(locx, locy):
+      self.grid[locx][locy] = direction
+    else:
+      # TODO:# log or throw exception
+      raise Exception(f"Rover insertion failed for : {locx},{locy}")
+
+  def is_location_available(self, locx: int, locy: int):
+    return locx < self.width and locy < self.length and not self.grid[locx][locy] 
