b71e8cc9c0ab67e7ab247982dc3ff1b964ee13c0
RehaS <beratson@gmail.com>
2020-07-18 23:10:15 +0200

    app.py update, command files update, tasklist update

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
-#!/usr/local/bin/python3 python3
+# !/usr/local/bin/python3
 """Main App Module"""
+import sys, os
+from modules.satellite_link import SatelliteLink
+
+if "mars-rover" in os.listdir():
+  os.chdir("mars-rover")
 
 print("Hello Mars Rover")
+
+COMMAND_FILE_PATH = "./command_files/command1.txt"
+
+mySat = SatelliteLink()
+
+mySat.get_commands_file(COMMAND_FILE_PATH)
+mySat.send_status().display()
+mySat.execute_commands()
+mySat.send_status().display()
