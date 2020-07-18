2ef4e86edaf6ef27e7bb637d81ab9d566efcf6e4
RehaS <beratson@gmail.com>
2020-07-18 16:45:52 +0200

    Satellite and Rover modules init

---
+++ modules/rover.py
@@ -0,0 +1,8 @@
+'''
+Rover module
+'''
+class Rover():
+  '''class to handle command executions on a rover'''
+  def __init__(self):
+    print("This initializes a Rover class")
+		
---
+++ modules/satellite.py
@@ -0,0 +1,14 @@
+'''
+Satellite module
+'''
+class Satellite():
+  '''class to handle communications between earth and mars,
+  orbiting around mars to oversee rovers
+  '''
+  def __init__(self):
+    print("This initializes a satellite class")
+    pass
+  def get_commands(self):
+    pass
+  def send_results(self):
+    pass
