64c4a2fc34a97b2420382f7041cbac368a955b4c
RehaS <beratson@gmail.com>
2020-07-19 13:00:00 +0200

    logging init

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 # !/usr/local/bin/python3
 """Main App Module"""
-import sys, os
+import sys
+import os
+import logging
+import datetime
 from modules.satellite_link import SatelliteLink
 
 if "mars-rover" in os.listdir():
   os.chdir("mars-rover")
 
 print("Hello Mars Rover")
-
-
 
 if __name__ == "__main__":
   print("main")
@@ -19,10 +20,13 @@ if __name__ == "__main__":
 
   COMMAND_FILE_PATH = f"./command_files/{FILE_NAME}"
 
+  logging.basicConfig(filename=f'./log_files/{FILE_NAME}.log', level=logging.INFO, filemode="w")
+  logging.info("Started Captain's Log on %s", datetime.datetime.now())
+
   mySat = SatelliteLink()
 
   mySat.get_commands_file(COMMAND_FILE_PATH)
   mySat.send_status().display()
   mySat.execute_commands()
   mySat.send_status().display()
-  pass
+  logging.info('Finished')
